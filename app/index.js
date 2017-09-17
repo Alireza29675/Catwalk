9c87d82c0e5285be4bd3a95435ffcc2c851b027d
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-18 03:03:26 +0430

    Application ordered very well!

--- app/index.js
+++ app/index.js
@@ -1,42 +1,4 @@
-'use strict';
-const electron = require('electron');
+import App from './App'
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const app = new App( __dirname + '/../public' );
 
-let indexFile = `${__dirname}/../public/index.html`;
-
-let mainWindow;
-
-function onClosed() {
-	mainWindow = null;
-}
-
-function createMainWindow() {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 500
-	});
-
-	win.loadURL(`file:${indexFile}`);
-
-	win.on('closed', onClosed);
-
-	return win;
-}
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-app.on('activate-with-no-open-windows', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
-});
-
-app.on('ready', () => {
-	mainWindow = createMainWindow();
-});
