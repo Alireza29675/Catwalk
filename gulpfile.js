34f72e42084bddbbc5831e0f2279ff89bdc80aea
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-09-19 18:52:32 +0430

    Some bugs fixed in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,23 @@ const webpackConfig = require('./webpack.config');
 
 gulp.task('serve', function () {
 	babelFiles();
-	// Electron Run
-	electron.start();
-	gulp.watch(['./lib/*.*', './lib/**/*.*'], () => {
-		console.log('App Restarted!');
-		electron.restart()
-	});
-	gulp.watch(['./public/*.*', './public/**/*.*'], () => {
-		console.log('App Reloaded!');
-		electron.reload()
-	});
-	// Webpack Front-end Watch
-	gulp.watch(['./src/*.*', './src/**/*.*'], createWebpackBundle);
-	// Babel-Watch Server Side
-	gulp.watch(['./app/*.*', './app/**/*.*'], babelFiles);
+	createWebpackBundle();
+	setTimeout(function () {
+		// Electron Run
+		electron.start();
+		gulp.watch(['./lib/*.*', './lib/**/*.*'], () => {
+			console.log('App Restarted!');
+			electron.restart()
+		});
+		gulp.watch(['./public/*.*', './public/**/*.*'], () => {
+			console.log('App Reloaded!');
+			electron.reload()
+		});
+		// Webpack Front-end Watch
+		gulp.watch(['./src/*.*', './src/**/*.*'], createWebpackBundle);
+		// Babel-Watch Server Side
+		gulp.watch(['./app/*.*', './app/**/*.*'], babelFiles);
+	}, 1000)
 });
 
 const createWebpackBundle = () => webpack(webpackConfig).run(result => console.log('New Bundle Created!'));
